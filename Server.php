3a1ef0e834f560351ca12ec4009d3872af3b6020
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-07-27 03:44:44 +0430

    add socket_read [server]

--- Server.php
+++ Server.php
@@ -44,6 +44,7 @@ set_time_limit(0);
    | domain = AF_INIT: IPv4 Internet based protocols. TCP and UDP are common protocols of this protocol family.
    | type = SOCK_STREAM: Provides sequenced, reliable, full-duplex, connection-based byte streams. An out-of-band data transmission mechanism may be supported. The TCP protocol is based on this socket type.
    | protocol = 0 , you can use tcp, udp or icmp
+   |
    */
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or
 die("Could not create socket" . PHP_EOL);
@@ -56,6 +57,7 @@ die("Could not create socket" . PHP_EOL);
    | socket_bind ( resource $socket , string $address [, int $port = 0 ] ) : bool
    | address = localhost
    | port = custom port for example 25003
+   |
    */
 
 $result = socket_bind($socket, $host, $port) or
@@ -68,6 +70,7 @@ die("Could not bind to socket" . PHP_EOL);
    | socket_listen ( resource $socket [, int $backlog = 0 ] ) : bool
    | backlog = A maximum of backlog incoming connections will be queued for processing.
    | for example we use 3
+   |
    */
 
 $result = socket_listen($socket, 3) or
@@ -80,8 +83,23 @@ die("Could not setup socket listener" . PHP_EOL);
    | socket_accept ( resource $socket ) : resource
    | accept incoming connections
    | spawn another socket to handle communication
+   |
    */
 
 $spawn = socket_accept($socket) or
 die("Could not accept incoming connection" . PHP_EOL);
+
+
+/*
+   |--------------------------------------------------------------------------
+   |   Reads a maximum of length bytes from the socket
+   |--------------------------------------------------------------------------
+   | socket_read ( resource $socket , int $length [, int $type = PHP_BINARY_READ ] ) : string
+   | length =  The maximum number of bytes read is specified by the length parameter
+   |
+   */
+
+$input = socket_read($spawn, 1024) or
+die("Could not read input". PHP_EOL);
+
 
