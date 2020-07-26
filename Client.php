5f2e253ce355c0bd32187fd2aa884d1a98136dde
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-07-27 03:51:20 +0430

    add socket_create[client]

--- Client.php
+++ Client.php
@@ -22,3 +22,15 @@ $host = "127.0.0.1";
 $port = 25003;
 $message = "Hello Mr Aghakhani";
 echo "Message To server : ".$message;
+/*
+   |--------------------------------------------------------------------------
+   |  Create socket
+   |--------------------------------------------------------------------------
+   | socket_create ( int $domain , int $type , int $protocol ) : resource
+   | domain = AF_INIT: IPv4 Internet based protocols. TCP and UDP are common protocols of this protocol family.
+   | type = SOCK_STREAM: Provides sequenced, reliable, full-duplex, connection-based byte streams. An out-of-band data transmission mechanism may be supported. The TCP protocol is based on this socket type.
+   | protocol = 0 , you can use tcp, udp or icmp
+   |
+   */
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or
+die("Could not create socket" . PHP_EOL);
